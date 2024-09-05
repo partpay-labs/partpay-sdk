d69c76cbfcdd2a1baff1b692aad5ed2d0302c0cb
emmanuel-uzoezie <uzoezieemmanuel@gmail.com>
2024-09-05 23:25:52 +0100

    change app module

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,24 +1,35 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ACTIONS_CORS_HEADERS } from '@solana/actions';
 
 async function bootstrap() {
-  try {
-    const app = await NestFactory.create(AppModule);
-    
-    // Enable CORS
-    app.enableCors({
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
-      credentials: true,
+  const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: Object.keys(ACTIONS_CORS_HEADERS),
+    credentials: true,
+  });
+  
+
+  app.use((req, res, next) => {
+    Object.keys(ACTIONS_CORS_HEADERS).forEach(header => {
+      res.header(header, ACTIONS_CORS_HEADERS[header]);
     });
+    next();
+  });
 
-    await app.listen(process.env.PORT || 3004, "0.0.0.0");
-  } catch (error) {
-    process.exit(1);
-  }
+  await app.listen(process.env.PORT || 3008)
 }
-
-bootstrap().catch(err => {
-  process.exit(1);
-});
+bootstrap();
--- api/src/pay/pay.module.ts
+++ api/src/pay/pay.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PayController } from './pay.controller';
 import { PayService } from './pay.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [PayController],
-  providers: [PayService]
+  providers: [PayService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ]
 })
 export class PayModule {}
