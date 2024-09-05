68ec222a34ca7336de39e29f8452f7a13e52806a
emmanuel-uzoezie <uzoezieemmanuel@gmail.com>
2024-09-05 23:05:42 +0100

    edited some

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,7 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    
+    // Enable CORS
+    app.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+      credentials: true,
+    });
+
+    await app.listen(process.env.PORT || 3004, "0.0.0.0");
+  } catch (error) {
+    process.exit(1);
+  }
 }
-bootstrap();
+
+bootstrap().catch(err => {
+  process.exit(1);
+});
